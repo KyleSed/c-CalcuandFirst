b981dfffcc25b66d085c6197a5f7efc303c6cf71
Kyle Zedrick Villamor <146597376+KyleSed@users.noreply.github.com>
2026-09-19 12:38:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter your first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your operation (+ , - , * , / , %): ");
+string operation = Console.ReadLine();
+
+if (operation == "+")
+{
+Console.WriteLine("Sum: " + (num1 + num2));
+}
+else if (operation == "-"){
+Console.WriteLine("Difference: " + (num1 - num2));
+}
+else if (operation == "*"){
+Console.WriteLine("Product: " + (num1 * num2));
+}
+else if (operation == "/"){
+Console.WriteLine("Difference: " + (num1 / num2));
+}
+else if (operation == "%"){
+Console.WriteLine("Remainder: " + (num1 % num2));
+}
+
